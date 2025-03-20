35534ef6630436a7953c7ffb6eff6e34d8c4fab8
AmineHammoutene10 <aminehammoutene10@gmail.com>
2025-03-21 00:02:50 +0100

    Tailwind style update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  template: `<div class="header">{{title ()}} </div> `,
+  imports: [PrimaryButtonComponent],
+  template: `<div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
+    <span>My store</span>
+    <app-primary-button  label="Cart" (btnClicked)="showButtonclicked()" />
+  </div> `,
   styles: `
-  
-
   
   `,
 })
 export class HeaderComponent {
 
+  showButtonclicked(){
+    console.log("clicked")
+  }
   title = signal('This is my app')
 
 }
---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: `
+    <button (click)="btnClicked.emit()">
+      {{ label() }}
+    </button>
+  `,
+  styles: ``
+})
+export class PrimaryButtonComponent {
+
+  label = input('') 
+
+  btnClicked = output();
+
+  
+  
+}
